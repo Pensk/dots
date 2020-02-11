4e76b96f85f7255cb330e3ea670e15be144e191a
pensk <pensk@hotmail.ca>
2020-02-11 14:03:47 +0900

    add fish config

---
+++ config/fish/config.fish
@@ -0,0 +1,12 @@
+export SSH_AUTH_SOCK="$XDG_RUNTIME_DIR/ssh-agent.socket"
+
+alias emacs='emacs -nw'
+
+alias gs='git status'
+alias gf='git fetch'
+alias gd='git diff'
+alias gc='git commit'
+
+alias pj='cd ~/proj; cd'
+alias pd='cd ~/proj/pd; cd'
+alias lb='cd ~/proj/labot; cd'
