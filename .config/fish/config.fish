0c73f40935bd6844c7fd54c459932d47345e0dbe
Pensk <superpensk@gmail.com>
2021-02-02 09:07:50 +0900

    update aliases

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,4 +1,4 @@
-export SSH_AUTH_SOCK="$XDG_RUNTIME_DIR/ssh-agent.socket"
+#export SSH_AUTH_SOCK="$XDG_RUNTIME_DIR/ssh-agent.socket"
 
 alias emacs='emacs -nw'
 
@@ -6,15 +6,18 @@ alias gs='git status'
 alias gf='git fetch'
 alias gd='git diff'
 alias gc='git commit'
+alias gch='git checkout'
 
 alias pj='cd ~/proj; cd'
-alias pd='cd ~/proj/pd; cd'
-alias lb='cd ~/proj/labot; cd'
+alias rf='cd ~/proj/rf; cd'
 
-eval (ssh-agent -c)
+# eval (ssh-agent -c)
 
-if status is-login
-    if test -z "$DISPLAY" -a $XDG_VTNR = 1
-        exec tdm
-    end
-end
+# if status is-login
+#     if test -z "$DISPLAY" -a $XDG_VTNR = 1
+#         exec tdm
+#     end
+# end
+
+# homebrew bins
+set -g fish_user_paths "/usr/local/sbin" $fish_user_paths
