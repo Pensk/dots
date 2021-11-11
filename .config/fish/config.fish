587332e45aa99f2c4bda91055cfe6cd3d8957455
Pensk <superpensk@gmail.com>
2021-11-11 13:55:30 +0900

    check for interactive status on fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,4 +1,4 @@
-#export SSH_AUTH_SOCK="$XDG_RUNTIME_DIR/ssh-agent.socket"
+# export SSH_AUTH_SOCK="$XDG_RUNTIME_DIR/ssh-agent.socket"
 
 alias emacs='emacs -nw'
 
@@ -11,14 +11,16 @@ alias gch='git checkout'
 alias pj='cd ~/proj; cd'
 alias rf='cd ~/proj/rf; cd'
 
-# eval (ssh-agent -c)
-# setxkbmap -model pc104 -layout us,us -variant ,dvorak -option grp:shift_toggle
+if status is-interactive
+  eval (ssh-agent -c)
+  setxkbmap -model pc104 -layout us,us -variant ,dvorak -option grp:shift_toggle
 
-# if status is-login
-#     if test -z "$DISPLAY" -a $XDG_VTNR = 1
-#         exec tdm
-#     end
-# end
+  if status is-login
+    if test -z "$DISPLAY" -a $XDG_VTNR = 1
+      exec tdm
+    end
+  end
+end
 
 # homebrew bins
 # set -g fish_user_paths "/usr/local/sbin" $fish_user_paths
