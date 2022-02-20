a98d7e9fbfab0e48bc4f6566786448b7478bae9b
pensk <superpensk@gmail.com>
2022-02-20 12:13:36 +0900

    Use emacsclient in alias

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,6 +1,8 @@
 # export SSH_AUTH_SOCK="$XDG_RUNTIME_DIR/ssh-agent.socket"
 
-alias emacs='emacs -nw'
+alias emacs='emacsclient --create-frame --alternate-editor=""'
+
+set -gx EDITOR "emacsclient -t"
 
 alias gs='git status'
 alias gf='git fetch'
@@ -9,17 +11,17 @@ alias gc='git commit -Sm'
 alias gch='git checkout'
 
 alias pj='cd ~/proj; cd'
-alias rf='cd ~/proj/rf; cd'
+alias op='cd ~/proj/opn; cd'
 
 if status is-interactive
   eval (ssh-agent -c)
   setxkbmap -model pc104 -layout us,us -variant ,dvorak -option grp:shift_toggle
 
-  if status is-login
-    if test -z "$DISPLAY" -a $XDG_VTNR = 1
-      exec tdm
-    end
-  end
+  # if status is-login
+  #   if test -z "$DISPLAY" -a $XDG_VTNR = 1
+  #     exec tdm
+  #   end
+  # end
 end
 
 # homebrew bins
