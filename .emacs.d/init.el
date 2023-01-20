76f587fa0c4ef15257eaa2517cdf3acec7a33206
Pensk <superpensk@gmail.com>
2023-01-21 00:34:43 +0900

    emacs config org

---
+++ .emacs.d/init.el
@@ -0,0 +1,21 @@
+(org-babel-load-file
+ (expand-file-name
+  "config.org"
+  user-emacs-directory))
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("7e377879cbd60c66b88e51fad480b3ab18d60847f31c435f15f5df18bdb18184" default))
+ '(org-agenda-files nil)
+ '(package-selected-packages
+   '(company lsp-mode writeroom-mode vterm projectile perspective org-bullets neotree magit lua-mode ivy-posframe smex ivy-rich counsel gcmh general doom-modeline evil-collection evil use-package)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
