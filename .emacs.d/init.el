bbba91b6012b2bd3ae6ed56962c099882ecce774
Pensk <superpensk@gmail.com>
2023-01-28 12:20:38 +0900

    auto updated init.el

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -9,10 +9,10 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
-   '("7e377879cbd60c66b88e51fad480b3ab18d60847f31c435f15f5df18bdb18184" default))
+   '("7a424478cb77a96af2c0f50cfb4e2a88647b3ccca225f8c650ed45b7f50d9525" "f458b92de1f6cf0bdda6bce23433877e94816c3364b821eb4ea9852112f5d7dc" "7e377879cbd60c66b88e51fad480b3ab18d60847f31c435f15f5df18bdb18184" default))
  '(org-agenda-files nil)
  '(package-selected-packages
-   '(company lsp-mode writeroom-mode vterm projectile perspective org-bullets neotree magit lua-mode ivy-posframe smex ivy-rich counsel gcmh general doom-modeline evil-collection evil use-package)))
+   '(yaml-mode typescript-mode rainbow-mode company lsp-mode writeroom-mode vterm projectile perspective org-bullets neotree magit lua-mode ivy-posframe smex ivy-rich counsel gcmh general doom-modeline evil-collection evil use-package)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
